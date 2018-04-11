cbd38c04cadfa4012f13a8a87063e465f5051620
Hardy Har <hardyhar@email.com>
2018-04-11 12:16:26 -0500

    Committing first time

---
+++ src/main/java/com/jeff/HelloTemplateApplication.java
@@ -0,0 +1,19 @@
+package com.jeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+
+@SpringBootApplication
+@Controller
+
+public class HelloTemplateApplication {
+
+	@RequestMa
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloTemplateApplication.class, args);
+	}
+}
